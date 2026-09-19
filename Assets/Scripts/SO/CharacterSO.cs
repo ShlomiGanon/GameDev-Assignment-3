769d6503b9c02eaa83df1bb5775bd9a05368d36f
Maor Buchbut <hellz35u@gmail.com>
2026-09-19 16:14:37 +0300

    adding a name field to the CharacterSO

--- Assets/Scripts/SO/CharacterSO.cs
+++ Assets/Scripts/SO/CharacterSO.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CharacterSO", menuName = "Scriptable Objects/CharacterSO", order = 0)]
 public class CharacterSO : ScriptableObject
 {
+    [Header("Info")]
+    [SerializeField] string characterName = "Name";
+    public string CharacterName { get => characterName; private set => characterName = value;}
+
     [Header("Movement")]
     [SerializeField] float moveSpeed = 10f;    
     public float MoveSpeed { get => moveSpeed;  private set => moveSpeed = value;}
