3a6784e95a18b0f97fef5026e3b2917ae487ee1d
Maor Buchbut <hellz35u@gmail.com>
2026-09-19 19:55:21 +0300

    created an NPC Script that handle all dialogue states also a scriptableobject script called NPCDialogue for contains dialogue data after that i created a canva for dialogue box that contains portrait of npc, name, dialogue text and end dialogue button

--- Assets/Scripts/NPC/NPC.cs
+++ Assets/Scripts/NPC/NPC.cs
@@ -1,16 +1,90 @@
 using UnityEngine;
-
-public class NPC : MonoBehaviour
+using TMPro;
+using UnityEngine.UI;
+using System.Collections;
+public class NPC : MonoBehaviour, IInteractable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] NPCDialogue dialogueData;
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText, nameText;
+    public Image portraitImage;
+
+    private int dialogueIndex;
+    private bool isTyping, isDialogueActive;
+
+    public void Interact()
     {
+        if (dialogueData == null || (/*PauseController.IsPaused && */ !isDialogueActive)) //we dont want when the dialogue keep going to show up on pause menu screen
+            return;
+        if(isDialogueActive)
+        {
+            NextLine();
+        }
+        else
+        {
+            StartDialogue();
+        }
+    }
+    void StartDialogue()
+    {
+        isDialogueActive = true;
+        dialogueIndex = 0;
+        nameText.SetText(dialogueData.npcName);
+        portraitImage.sprite = dialogueData.npcPortrait;
+        dialoguePanel.SetActive(true);
+        //PauseController.SetPause(true);
+
+        StartCoroutine(TypeLine());
         
     }
-
-    // Update is called once per frame
-    void Update()
+    void NextLine()
     {
-        
+        if(isTyping)
+        {
+            StopAllCoroutines();
+            dialogueText.SetText(dialogueData.dialogueLines[dialogueIndex]);
+            isTyping = false;
+        }
+        else if(++dialogueIndex < dialogueData.dialogueLines.Length)
+        {
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    IEnumerator TypeLine()
+    {
+        isTyping = true;
+        dialogueText.SetText("");
+
+        foreach (char letter in dialogueData.dialogueLines[dialogueIndex])
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(dialogueData.typingSpeed);
+        }
+
+        isTyping = false;
+
+        if (dialogueData.autoProgressLines.Length > dialogueIndex && dialogueData.autoProgressLines[dialogueIndex])
+        {
+            yield return new WaitForSeconds(dialogueData.autoProgressDelay);
+            NextLine();
+        }
+
+    }
+    public void EndDialogue()
+    {
+        StopAllCoroutines();
+        isDialogueActive = false;
+        dialogueText.SetText("");
+        dialoguePanel.SetActive(false);
+        //pausecontroller set false
+    }
+    public bool CanInteract()
+    {
+        return !isDialogueActive;
     }
 }
